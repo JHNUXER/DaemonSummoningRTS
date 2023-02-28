6bdb0f5efe8f4d926eb284698dddb30fc4ff9f36
JHNUXER <johnhoganuke2@gmail.com>
2023-02-28 04:54:32 -0500

    Moved collision logic classes to Collision.js
    Fixed AABB2 and Circle to use a common Collider2D base class.
    Added proper overloads.

--- game.js
+++ game.js
@@ -144,61 +144,6 @@ class Daemon extends Creature {
   
 }
 
-
-class AABB2 {
-  
-  get ax() { return this.a.x; }
-  get ay() { return this.a.y; }
-  get bx() { return this.b.x; }
-  get by() { return this.b.y; }
-  get cx() { return (this.b.x - this.a.x) * 0.5 + this.a.x; }
-  get cy() { return (this.b.y - this.a.y) * 0.5 + this.a.y; }
-  
-  constructor(ax, ay, bx, by) {
-    this.a = new Vec3(ax, ay, 0) ;
-    this.b = new Vec3(bx, by, 0) ;
-  }
-  
-  overlaps(other) {
-    if (other instanceof AABB2) {
-      return !(this.ax  > other.bx || this.ay  > other.by
-        ||     other.ax > this.bx  || other.ay > this.by) ;
-    } else if (other instanceof Circle) {
-      let cx = other.x      ;
-      let cy = other.y      ;
-      let cr = other.radius ;
-      let rr = cr * cr      ;
-      
-      let dx = cx - Math.max(this.ax, Math.min(cx, this.bx)) ;
-      let dy = cy - Math.max(this.ay, Math.min(cy, this.by)) ;
-    }
-  }
-  
-}
-class Circle {
-  
-  constructor(x, y, r) {
-    this.x      = x ;
-    this.y      = y ;
-    this.radius = r ;
-  }
-  
-  overlaps(other) {
-    if (other instanceof Circle) {
-      let rs = this.radius + other.radius ;
-      let rs2 = rs * rs ;
-      let dx  = this.x - other.x ;
-      let dy  = this.y - other.y ;
-      
-      return dx * dx + dy * dy <= rs2 ;
-    } else if (other instanceof AABB2) {
-      return other.overlaps(this) ;
-    }
-  }
-  
-}
-
-
 class Ent extends Creature {
   
   constructor(x, y, z) {
